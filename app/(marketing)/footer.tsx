899d932b6565efc7a8a03666dc8959bd1bf0115c
Ali Yorulmaz <aliyorulmazdev@gmail.com>
2024-03-28 16:06:40 +0300

    Courses lessons

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,5 +1,41 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
 export const Footer = () => {
-    return (
-        <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">Footer</footer>
-    )
-}
+  return (
+    <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
+      <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image
+            src="/gr.svg"
+            alt="Deutsch"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Deutsch
+        </Button>
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image
+            src="/uk.svg"
+            alt="English"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          English
+        </Button>
+        <Button size="lg" variant="ghost" className="w-full">
+          <Image
+            src="/fr.svg"
+            alt="Français"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Français
+        </Button>
+      </div>
+    </footer>
+  );
+};
---
+++ scripts/seed.ts
@@ -0,0 +1,138 @@
+import "dotenv/config";
+const { drizzle } = require("drizzle-orm/neon-http");
+
+
+import { neon } from "@neondatabase/serverless";
+
+import * as schema from "../db/schema";
+
+const sql = neon(process.env.DATABASE_URL!);
+const db = drizzle(sql, { schema });
+
+const main = async () => {
+  try {
+    console.log("Seeding database");
+    await db.delete(schema.courses);
+    await db.delete(schema.userProgress);
+    await db.delete(schema.units);
+    await db.delete(schema.lessons);
+    await db.delete(schema.challenges);
+    await db.delete(schema.challengeOptions);
+    await db.delete(schema.challengeProgress);
+
+    await db.insert(schema.courses). values ([
+        {
+            id: 1,
+            title: "English",
+            imageSrc: '/UK.svg'
+        },
+        {
+            id: 2,
+            title: "Deutsch",
+            imageSrc: '/gr.svg'
+        },
+        {
+            id: 3,
+            title: "French",
+            imageSrc: '/fr.svg'
+        },
+    ]);
+
+    await db.insert(schema.units).values([
+      {
+        id: 1,
+        courseId:1,
+        title: "English Unit 1",
+        description: "Learn the basics of English",
+        order: 1
+      }
+    ]);
+
+    await db.insert(schema.lessons).values([
+      {
+        id: 1,
+        unitId: 1,
+        order: 1,
+        title: 'Numbers',        
+      },
+      {
+        id: 2,
+        unitId: 1,
+        order: 2,
+        title: 'Words',        
+      }
+    ]);
+
+    await db.insert(schema.challenges).values([
+      {
+        id: 1,
+        lessonId: 1,
+        type: 'SELECT',
+        order: 1,
+        question: "Aşağıdakilerden hangisi 4(dört) sayısıdır.",
+      },
+      {
+        id: 2,
+        lessonId: 2,
+        type: 'SELECT',
+        order: 1,
+        question: "Aşağıdakilerden hangisi erkek çocuktur?",
+      }
+    ]);
+
+    await db.insert(schema.challengeOptions).values([
+      {
+        challengeId: 1,
+        imageSrc: '/four.svg',
+        correct: true,
+        text: 'Four',
+        audioSrc: '/four.mp3'
+      },
+      {
+        challengeId: 1,
+        imageSrc: '/one.svg',
+        correct: false,
+        text: 'One',
+        audioSrc: '/one.mp3'
+      },
+      {
+        challengeId: 1,
+        imageSrc: '/two.svg',
+        correct: false,
+        text: 'Two',
+        audioSrc: '/two.mp3'
+      },
+    ]),
+    await db.insert(schema.challengeOptions).values([
+      {
+        challengeId: 2,
+        imageSrc: '/woman.svg',
+        correct: false,
+        text: 'Woman',
+        audioSrc: '/woman.mp3'
+      },
+      {
+        challengeId: 2,
+        imageSrc: '/man.svg',
+        correct: false,
+        text: 'Man',
+        audioSrc: '/man.mp3'
+      },
+      {
+        challengeId: 2,
+        imageSrc: '/boy.svg',
+        correct: true,
+        text: 'Boy',
+        audioSrc: '/boy.mp3'
+      },
+    ])
+
+    console.log("Seeding finished");
+  } catch (error) {
+    console.error(error);
+    throw new Error("Failed to seed database");
+  }
+};
+
+
+main();
